0c97c443c33147d53ab88006bbf9e1be3cfbb192
JK1320 <kaurjus1@gmail.com>
2022-04-22 08:53:54 +0100

    testing button with checkbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 const [buttonColor, setButtonColor] = useState('red');
+const [disabled, setDisabled] = useState(false);
 
 const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
@@ -12,9 +12,18 @@ const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
       <button
         onClick={() => setButtonColor(newButtonColor)}
         style={{ backgroundColor: buttonColor }}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}    // for screen readers
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
